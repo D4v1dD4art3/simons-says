9b7791279fc2cead361dac31fb215df194d56e0c
D4v1d_D4art3 <jesusdavidduartejimenez@gmail.com>
2020-09-07 19:03:34 -0400

    creacion de animaciones y linkes

--- simon/index.js
+++ simon/index.js
@@ -3,8 +3,8 @@ const celeste = document.getElementById('celeste');
 const verde = document.getElementById('verde');
 const naranja = document.getElementById('naranja');
 const btnEmpezar = document.getElementById('btnEmpezar');
+const navbarLinks = document.querySelectorAll(".navbar a");
 const ULTIMO_NIVEL = 10;
-
 
 class Juego {
     constructor(){
@@ -124,4 +124,81 @@ function empezarJuego() {
    window.juego = new Juego();
 }
 
+// smoothScroll
 
+for(let i=0; i < navbarLinks.length; i++) {
+    navbarLinks[i].addEventListener("click", navbarLinkClick);
+  }
+  
+  function navbarLinkClick(event) {
+  
+    smoothScroll(event); // Call the "smoothScroll" function
+    
+  }
+  // Smooth-Scrolling
+
+// APPROACH #1 - window.scrollTo() (window.scroll())
+// function smoothScroll(event) {
+//   event.preventDefault();
+//   const targetId = event.currentTarget.getAttribute("href");
+//   window.scrollTo({
+//     top: targetId==="#" ? 0 : document.querySelector(targetId).offsetTop,
+//     behavior: "smooth"
+//   });
+// }
+
+// APPROACH #2 - element.scrollIntoView()
+// function smoothScroll(event) {
+//   event.preventDefault();
+//   const targetId = event.currentTarget.getAttribute("href")==="#" ? "header" : event.currentTarget.getAttribute("href");
+//   document.querySelector(targetId).scrollIntoView({
+//     behavior: "smooth",
+//     block: "start"
+//   });
+// }
+
+// APPROACH #3 - window.requestAnimationFrame()
+function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute("href")==="#" ? "header" : event.currentTarget.getAttribute("href");
+    const altoDelHeader = 110;
+    const targetPosition = document.querySelector(targetId).offsetTop - altoDelHeader;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1500;
+    let start = null;
+    console.log(startPosition);
+    window.requestAnimationFrame(step);
+  
+    function step(timestamp) {
+        console.log(timestamp)
+      if (!start) start = timestamp;
+
+      const progress = timestamp - start;
+      //  aqui van las animaciones 
+      // window.scrollTo(0, distance*(progress/duration) + startPosition);
+      window.scrollTo(0, easeInOutQuad(progress, startPosition, distance, duration) - 1);
+
+      if (progress < duration) window.requestAnimationFrame(step);
+    }
+  }
+  
+  // Easing Functions en estas funciones se pasan los mismos parametros que easeInoutCubic
+  
+  function linear(t, b, c, d) {
+      return c*t/d + b;
+  };
+  
+  function easeInOutQuad(t, b, c, d) {
+      t /= d/2;
+      if (t < 1) return c/2*t*t + b;
+      t--;
+      return -c/2 * (t*(t-2) - 1) + b;
+  };
+  
+  function easeInOutCubic(t, b, c, d) {
+      t /= d/2;
+      if (t < 1) return c/2*t*t*t + b;
+      t -= 2;
+      return c/2*(t*t*t + 2) + b;
+  };
